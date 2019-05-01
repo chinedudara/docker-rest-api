475a9a28fb62f4d03e85425637dca032814c2379
Chinedu Dara <darachinedu@gmail.com>
2019-05-01 19:52:09 +0100

    introduced an error to add function to cause fail on commit

--- src/functions.js
+++ src/functions.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const functions = {
-    add: (a, b) => a + b,
+    add: (a, b) => a + b + 20,
     isNull: () => null,
     checkValue: (a) => a,
     sayName: () => 'Chinedu Dara',
